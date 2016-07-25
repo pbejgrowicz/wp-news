c41cfaf7f9b149aea61a7fb84220847f0d043060
Speednet <user1@mac-mini-user.local>
2016-07-25 15:25:09 +0200

    Initial Commit

---
+++ wppl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wppl
+//
+//  Created by Speednet on 25.07.2016.
+//  Copyright © 2016 Speednet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wppl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wppl
+//
+//  Created by Speednet on 25.07.2016.
+//  Copyright © 2016 Speednet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
